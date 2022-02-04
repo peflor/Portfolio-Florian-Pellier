b9848d86e470949d339e8d2332c1180f3429b943
Florian <92161528+peflor@users.noreply.github.com>
2022-02-04 11:43:15 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,36 +1,43 @@
 let villeChoisie;
 // recevoirTemperature(villeChoisie);
 
-if ("geolocation" in navigator) {
-    navigator.geolocation.watchPosition((position) =>{
-        const url = 'https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&appid=59720dc89a1714b9ab598f9476bef072&units=metric';
-        console.log(url);
-        let requete = new XMLHttpRequest();
-        requete.open ('GET', url),
-        requete.responseType = 'json';
-        requete.send();
+// Avec Géolocalisation
+
+// if ("geolocation" in navigator) {
+//     navigator.geolocation.watchPosition((position) =>{
+//         const url = 'https://api.openweathermap.org/data/2.5/weather?lon=' + position.coords.longitude + '&lat=' + position.coords.latitude + '&appid=59720dc89a1714b9ab598f9476bef072&units=metric';
+//         console.log(url);
+//         let requete = new XMLHttpRequest();
+//         requete.open ('GET', url),
+//         requete.responseType = 'json';
+//         requete.send();
 
 
-        requete.onload = function () {
-            if (requete.readyState === XMLHttpRequest.DONE) {
-                if (requete.status === 200) {
-                    let reponse = requete.response ;
-                    let temperature = reponse.main.temp;
-                    let ville = reponse.name;
-                    document.querySelector('#temperature_label').textContent = temperature;
-                    document.querySelector('#ville').textContent = ville;
-                }
-                else {
-                    alert('Un problème est intervenu, merci de revenir plus tard.');
-                }
-            }
-        }
-    }, erreur, options);
-}
-else {
-    villeChoisie = 'Paris';
+//         requete.onload = function () {
+//             if (requete.readyState === XMLHttpRequest.DONE) {
+//                 if (requete.status === 200) {
+//                     let reponse = requete.response ;
+//                     let temperature = reponse.main.temp;
+//                     let ville = reponse.name;
+//                     document.querySelector('#temperature_label').textContent = temperature;
+//                     document.querySelector('#ville').textContent = ville;
+//                 }
+//                 else {
+//                     alert('Un problème est intervenu, merci de revenir plus tard.');
+//                 }
+//             }
+//         }
+//     }, erreur, options);
+// }
+// else {
+//     villeChoisie = 'Paris';
+//     recevoirTemperature(villeChoisie);
+// }
+
+// Sans Géolocalisation
+
+villeChoisie = 'Paris';
     recevoirTemperature(villeChoisie);
-}
 
 var options = {
     enableHighAccuracy: true
